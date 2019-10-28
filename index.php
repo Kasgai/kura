5563fb3a79af759dde2ce86ce433f84059653d6c
Yuji Sasaki <jyu0414@gmail.com>
2019-10-28 14:22:30 +0900

    get image

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
-echo "Hello, World! ver2";
+echo "Hello, World! ver2\n";
 if(isset($_POST['html'])){
-    $comment = $_POST['html'];
-    echo $comment;
+    $html = $_POST['html'];
+    $domDoc = new DOMDocument();
+    $domDoc->loadXML($html);
+    $imgs = $domDoc->getElementsByTagName("img");
+    foreach($imgs as $img) {
+        echo $img->getAttribute("src");
+        echo "\n";
+    }
+    echo $html;
 } else {
-    echo "Not Available POST Data";
+    echo "Not Available POST Data\n";
 }
 ?>
